4d755b96bc1a2f8efd07ab8e6fc3f11cc6e4ad6b
Robert Warner <radicalphoenix@gmail.com>
2013-04-12 23:04:58 -0700

    consolidated config and settings

--- config.py
+++ config.py
@@ -1,3 +1,21 @@
+import os
+
+DEBUG = True
+
+#TODO: automate these with a build tool or something
+BUILD = 1
+VERSION = '0.0.1'
+
+# general
+BASE_DIR = os.path.dirname(__file__)
+
+# web app
+HOST = 'localhost'
+PORT = 8000
+STATIC_PATH = 'static'
+TEMPLATES_DIR = 'templates'
+
+# stats parser
 PLAY_BY_PLAY_URL = 'http://www.nhl.com/scores/htmlreports/20122013/PL02{game_id:04d}.HTM'
 PLAY_BY_PLAY_DIR = 'playbyplay_reports'
 DATA_DIR = 'generated_reports'
